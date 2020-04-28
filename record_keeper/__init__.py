ac938727891b9843cb3e8e6bd72e01c4934a2f84
TakeshiMusgrave <tkm45@cornell.edu>
2020-04-28 17:23:26 -0400

    v0.9.24: only import matplotlib if necessary

--- record_keeper/__init__.py
+++ record_keeper/__init__.py
@@ -1,2 +1,2 @@
-__version__ = "0.9.23"
+__version__ = "0.9.24"
 from .record_keeper import RecordKeeper, RecordWriter
--- record_keeper/record_keeper.py
+++ record_keeper/record_keeper.py
@@ -1,8 +1,4 @@
 #! /usr/bin/env python3
-
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
 from . import utils as c_f
 from .db_utils import DBManager
 import collections
@@ -14,18 +10,24 @@ import datetime
 
 
 class RecordKeeper:
-    def __init__(self, tensorboard_writer=None, record_writer=None, attributes_to_search_for=None):
+    def __init__(self, tensorboard_writer=None, record_writer=None, attributes_to_search_for=None, save_figures=False):
         self.tensorboard_writer = tensorboard_writer
         self.record_writer = record_writer
         self.attributes_to_search_for = [] if attributes_to_search_for is None else attributes_to_search_for
+        self.save_figures = save_figures
+        if save_figures:
+            import matplotlib
+            matplotlib.use('Agg')
+            import matplotlib.pyplot as plt
+            self.plt_module = plt
 
     def append_data(self, group_name, series_name, value, iteration):
-        if self.tensorboard_writer is not None:
+        if self.tensorboard_writer:
             tag_name = '%s/%s' % (group_name, series_name)
             if not c_f.is_list_and_has_more_than_one_element(value):
                 if not isinstance(value, datetime.datetime):
                     self.tensorboard_writer.add_scalar(tag_name, value, iteration)
-        if self.record_writer is not None:
+        if self.record_writer:
             self.record_writer.append(group_name, series_name, value)
 
     def update_records(self, record_these, global_iteration, custom_attr_func=None, input_group_name_for_non_objects=None, recursive_types=None):
@@ -70,8 +72,8 @@ class RecordKeeper:
             record_name += '_%s' % key_name
         return record_name
 
-    def maybe_add_custom_figures_to_tensorboard(self, global_iteration):
-        if self.record_writer is not None:
+    def maybe_add_multi_line_plots_to_tensorboard(self, global_iteration):
+        if self.record_writer and self.tensorboard_writer and self.save_figures:
             for group_name, dict_of_lists in self.record_writer.records.items():
                 for series_name, v in dict_of_lists.items():
                     if len(v) > 0 and isinstance(v[0], list):
@@ -83,22 +85,23 @@ class RecordKeeper:
         # Each sublist represents a snapshot at an iteration.
         # Transpose so that each row covers many iterations.
         numpified = np.transpose(np.array(list_of_lists))
-        fig = plt.figure()
+        fig = self.plt_module.figure()
         for sublist in numpified:
-            plt.plot(np.arange(numpified.shape[1]), sublist)
+            self.plt_module.plot(np.arange(numpified.shape[1]), sublist)
         return fig
 
     def add_embedding_plot(self, embeddings, labels, tag, global_iteration):
         # The pytorch tensorboard function "add_embedding" doesn't seem to work
         # So this will have to do for now
-        label_set = np.unique(labels)
-        num_classes = len(label_set)
-        fig = plt.figure()
-        plt.gca().set_prop_cycle(cycler("color", [plt.cm.nipy_spectral(i) for i in np.linspace(0, 0.9, num_classes)]))
-        for i in range(num_classes):
-            idx = labels == label_set[i]
-            plt.plot(embeddings[idx, 0], embeddings[idx, 1], ".", markersize=1)
-        self.tensorboard_writer.add_figure(tag, fig, global_iteration)
+        if self.tensorboard_writer and self.save_figures:
+            label_set = np.unique(labels)
+            num_classes = len(label_set)
+            fig = self.plt_module.figure()
+            self.plt_module.gca().set_prop_cycle(cycler("color", [self.plt_module.cm.nipy_spectral(i) for i in np.linspace(0, 0.9, num_classes)]))
+            for i in range(num_classes):
+                idx = labels == label_set[i]
+                self.plt_module.plot(embeddings[idx, 0], embeddings[idx, 1], ".", markersize=1)
+            self.tensorboard_writer.add_figure(tag, fig, global_iteration)
 
     def get_record(self, group_name):
         return self.record_writer.records[group_name]
