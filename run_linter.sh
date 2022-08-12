40be061f50f957b3d25ef1e7eced317d53969a96
KevinMusgrave <tkm45@cornell.edu>
2022-08-12 08:42:32 -0400

    Added .flake8 file

--- run_linter.sh
+++ run_linter.sh
@@ -1,2 +1,1 @@
-flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
-flake8 . --count --select=F401 --statistics --per-file-ignores="__init__.py:F401"
+flake8 . --count --show-source --statistics
